1cdc8feded42924876cd4b068cd4847d19ad542c
mephistopheies <pavel.nesterov@gmail.com>
2017-01-29 18:15:10 +0400

    added conditional gan

--- rlib/gan/__init__.py
+++ rlib/gan/__init__.py
@@ -1,4 +1,9 @@
 from .dcgan import *
+from .dcgan_discriminators import *
+from .dcgan_generators import *
+from .cdcgan import *
+from .cdcgan_discriminators import *
+from .cdcgan_generators import *
 from .helper import sample_uniform_noise, sample_normal_noise
 from .resnet import add_residual_block
 
@@ -6,4 +11,6 @@ __all__ = [
     'create_johnson_generator', 'create_radford_generator', 'create_discriminator_conv5',
     'create_johnson_generator_fc', 'create_redford_resnet_generator', 'create_discriminator_resnet',
     'sample_uniform_noise', 'sample_normal_noise', 'DCGAN', 'create_discriminator_resnet_small',
-    'add_residual_block', 'create_radford_generator_fc', 'create_discriminator_conv13']
+    'add_residual_block', 'create_radford_generator_fc', 'create_discriminator_conv13',
+    'create_discriminator_conv8',
+    'create_cond_discriminator_conv5', 'create_cond_radford_generator_fc', 'CDCGAN']
